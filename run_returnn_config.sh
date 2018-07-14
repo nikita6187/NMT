76a2c3d2072d66d8cfe7465d296d24777133cef8
nikita68 <n.makarov@hotmail.com>
2018-07-14 16:05:14 +0200

    Cudnn script

---
+++ run_returnn_config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH=~/cuda-9.0/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/cuda-9.0/extras/CUPTI/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/cuda-9.0/lib64
+
+source ./ENV/bin/activate
+
+python3 ../returnn/rnn.py $1
+
+
