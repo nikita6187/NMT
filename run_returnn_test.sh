3ebf40d91ca4980d9d85e40a5da53dc43a2466c1
nikita68 <n.makarov@hotmail.com>
2018-09-16 09:11:51 +0200

    Bug search continues

---
+++ run_returnn_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH=~/cuda-9.0/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/cuda-9.0/extras/CUPTI/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/cuda-9.0/lib64
+
+source ../rimes-testing/ENV/bin/activate
+
+python3 ../returnn_test/rnn.py $1
+
+
