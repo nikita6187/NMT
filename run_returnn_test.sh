401e0acb40e8b61478d454b3e1d7bb8446eafa60
nikita68 <n.makarov@hotmail.com>
2018-09-26 16:48:26 +0200

    HF: testing out different RETRUNN versions

--- run_returnn_test.sh
+++ run_returnn_test.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/cuda-9.0/lib64
 
 source ../rimes-testing/ENV/bin/activate
 
-python3 ../returnn-august/rnn.py $1
+python3 /u/bahar/src/python/returnn-hmm/returnn/rnn.py $1
 
 
