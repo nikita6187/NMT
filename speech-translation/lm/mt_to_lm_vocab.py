7d04c28b3d75dbfa5056401a10932a09743ba36a
nikita68 <n.makarov@hotmail.com>
2018-11-21 10:40:46 +0100

    ST LM: Vocab for MT

--- speech-translation/lm/mt_to_lm_vocab.py
+++ speech-translation/lm/mt_to_lm_vocab.py
@@ -1,7 +1,7 @@
 import pickle
 
 
-def convert_vocab(source_file, target_file):
+def convert_vocab_mt_to_lm(source_file, target_file):
     with open(source_file, "rb") as f:
         vocab = pickle.load(f)
         lines = []
@@ -11,4 +11,16 @@ def convert_vocab(source_file, target_file):
             wf.writelines(lines)
 
 
-convert_vocab('./target.vocab.pkl', './lm.vocab.txt')
+def convert_vocab_lm_to_mt(source_file, target_file):
+    with open(source_file, "rb") as f:
+        vocab_lines = f.readlines()
+    vocab = {}
+    for line in vocab_lines:
+        d = line.split()
+        vocab[d[0]] = int(d[1])
+    pickle.dump(vocab, open(target_file, "wb"))
+
+
+convert_vocab_lm_to_mt('./lm.vocab.txt', './newvocab.target.vocab.pkl')
+
+#convert_vocab('./target.vocab.pkl', './lm.vocab.txt')
