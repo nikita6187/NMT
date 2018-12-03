3b6db8304208ff8416f81631c25f1b7461f4680f
nikita68 <n.makarov@hotmail.com>
2018-12-03 15:54:25 +0100

    HF: De-en search

--- run_returnn_hmm_fac_search.sh
+++ run_returnn_hmm_fac_search.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/lib64
 
 source /u/makarov/rimes-testing/ENV/bin/activate
 
-# Usage: sh run_returnn_hmm_fac_search <Config> <Epoch Nr> <Output> <Log>
+# Usage: sh run_returnn_hmm_fac_search <Config> <Epoch Nr> <Output> <Log> <"dev"/"eval"/"test">
 
-python3 /u/makarov/returnn-hmm-fac/rnn.py $1 ++load_epoch $2 ++device 'gpu' --task 'search' ++search_data 'config:dev' ++beam_size '12' ++need_data 'False' ++max_seq_length '0' ++search_output_file $3 &> $4
+python3 /u/makarov/returnn-hmm-fac/rnn.py $1 ++load_epoch $2 ++device 'gpu' --task 'search' ++search_data 'config:'$5 ++beam_size '12' ++need_data 'False' ++max_seq_length '0' ++search_output_file $3 &> $4$5
