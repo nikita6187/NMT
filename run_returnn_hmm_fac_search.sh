b06d24dd26db65e6ae52857ddb8c416d14de9cce
nikita68 <n.makarov@hotmail.com>
2018-12-03 21:03:42 +0100

    Update run_returnn_hmm_fac_search.sh

--- run_returnn_hmm_fac_search.sh
+++ run_returnn_hmm_fac_search.sh
@@ -6,6 +6,6 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/lib64
 
 source /u/makarov/rimes-testing/ENV/bin/activate
 
-# Usage: sh run_returnn_hmm_fac_search <Config> <Epoch Nr> <Output> <Log> <"dev"/"eval"/"test">
+# Usage: sh run_returnn_hmm_fac_search <Config> <Epoch Nr> <Output> <Log> <name_of_dataset_variable_in_config>
 
 python3 /u/makarov/returnn-hmm-fac/rnn.py $1 ++load_epoch $2 ++device 'gpu' --task 'search' ++search_data "config:${5}" ++beam_size '12' ++need_data 'False' ++max_seq_length '0' ++search_output_file $3$5 &> $4$5
