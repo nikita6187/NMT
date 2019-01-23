73214de00aaa020093fed9470d2a38bf17a2bc7e
nikita68 <n.makarov@hotmail.com>
2019-01-23 09:58:14 +0100

    Launcher

--- launch_config.py
+++ launch_config.py
@@ -37,7 +37,8 @@ def main(args):
 
     print('Running: ' + launch_command + ' from ' + config_dir)
 
-    subprocess.Popen(launch_command, cwd=config_dir)
+    #subprocess.Popen(launch_command, cwd=config_dir)
+    subprocess.check_call(launch_command.split(), cwd=config_dir)
     print('Launched!')
 
 
