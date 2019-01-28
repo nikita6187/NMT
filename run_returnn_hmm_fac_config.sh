dca894af6b27ac4991f49ce62c8c2bd2f3aacc85
nikita68 <n.makarov@hotmail.com>
2019-01-28 20:20:05 +0100

    TF source

--- run_returnn_hmm_fac_config.sh
+++ run_returnn_hmm_fac_config.sh
@@ -4,7 +4,10 @@ export PATH=/u/makarov/cuda-9.0/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/extras/CUPTI/lib64
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/lib64
 
-source /u/makarov/rimes-testing/ENV/bin/activate
+#source /u/makarov/rimes-testing/ENV/bin/activate
+
+source /u/bahar/settings/python3-returnn-tf1.9/bin/activate
+
 
 python3 /u/makarov/returnn-hmm-fac/rnn.py $1
 
