c0bd078bb8750091fdc7c9bcd04bb1a11114626a
nikita68 <n.makarov@hotmail.com>
2019-01-30 15:08:14 +0100

    DIfferent CUDA version

--- run_returnn_hmm_fac_config.sh
+++ run_returnn_hmm_fac_config.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-export PATH=/u/makarov/cuda-9.0/bin:$PATH
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/extras/CUPTI/lib64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/lib64
+
+#export PATH=/u/makarov/cuda-9.0/bin:$PATH
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/extras/CUPTI/lib64
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/makarov/cuda-9.0/lib64
 
 #source /u/makarov/rimes-testing/ENV/bin/activate
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-9.0/lib64:/usr/local/cudnn-9.0-v7.1/lib64
 
 source /u/bahar/settings/python3-returnn-tf1.9/bin/activate
 
