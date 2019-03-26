63e3e8b4e7b9666861d0ac1346b02b8d40e7ddcb
nikita68 <n.makarov@hotmail.com>
2019-03-26 11:22:11 +0100

    TF: Domain mapping

--- topic-factorization/de-en/mapping-generation/mg_domain_segmentation.py
+++ topic-factorization/de-en/mapping-generation/mg_domain_segmentation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import gzip
 import pickle
+import copy
 
 # Paths, only on target training dataset!
 VOCAB_FILE = "/work/smt2/bahar/experiment/data-raw/vocabs-bpe20k/target.vocab.pkl"
@@ -71,8 +72,43 @@ for t1 in range(amount_of_domains):
         overlap[t1, t2] = len(set(vocab_distribution[t1]).intersection(set(vocab_distribution[t2])))
 print(overlap)
 
-# TODO: add <S> and etc to each vocab or make separate topic for them
 
+# Create topics out of distributions
+topics = copy.copy(vocab_distribution)
+
+# Add words which were not seen in training sets to all vocabs
+all_words_training_set = []
+for t in vocab_distribution:
+    for w in t:
+        all_words_training_set.append(w)
+all_words_training_set = set(all_words_training_set)
+all_words_vocab = set(vocab.keys())
+all_words_non_training = list(all_words_vocab.difference(all_words_training_set))
+print("Len of words not in training set: " + str(all_words_non_training))
+topics = [t.extend(all_words_non_training) for t in topics]
+
+# fill up with most common words to make them same size
+full_value = max([len(t) for t in topics])
+words_ordered_list = list(vocab.values())
+words_ordered_list.sort(key=lambda x: x[1])
+words_ordered_list = [w[0] for w in words_ordered_list]
+
+for t, idx in zip(topics, range(len(topics))):
+    amount_to_fill = full_value - len(t)
+    topics[idx].extend(copy.deepcopy(words_ordered_list)[0:amount_to_fill])
+
+print("Amount of vocab in each topic: ")
+print([len(dis) for dis in vocab_distribution])
+
+# add <S> and etc to each vocab
+meta_to_add = ["</S>", "<UNK>", "<S>"]
+topics = [t.extend(meta_to_add) for t in topics]
+
+print(topics)
+
+# TODO: convert to indices
+
+# TODO: save in correct format
 
 
 
